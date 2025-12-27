b542119bb334b56ab76b330d25931e31a45af218
yehyasalman <salmanyehia732@gmail.com>
2025-12-27 12:19:23 +0200

    Update chatbot responses and data preview display

--- pages/Ai.py
+++ pages/Ai.py
@@ -8,6 +8,9 @@ def load_data():
 
 df = load_data()
 
+st.subheader("Data Preview")
+st.dataframe(df)
+
 cat_cols = ['Category', 'Payment Method', 'Location', 'month', 'weekday']
 num_cols = ['Price Per Unit', 'Quantity', 'Discount Applied']
 
@@ -43,11 +46,11 @@ def chatbot_response(user_input):
 
     elif matched_intent == "avg_price":
         avg_price = df['Price Per Unit'].mean()
-        return f"The average price per unit is {avg_price:.2f}."
+        return f"The average price per unit is {avg_price:.2f}$."
 
     elif matched_intent == "discount":
-        avg_discount = df['Discount Applied'].mean()
-        return f"The average discount applied is {avg_discount:.2f}%."
+        avg_discount = df['Discount Applied'].mode()
+        return f"The average discount applied is {avg_discount}."
 
     elif matched_intent == "payment":
         top_payment = df['Payment Method'].value_counts().idxmax()
@@ -57,20 +60,12 @@ def chatbot_response(user_input):
         top_location = df['Location'].value_counts().idxmax()
         return f"The busiest location is {top_location}."
 
-    elif matched_intent == "weekday":
-        top_day = df['weekday'].value_counts().idxmax()
-        return f"Most sales happen on {top_day}."
-
-    elif matched_intent == "month":
-        top_month = df['month'].value_counts().idxmax()
-        return f"The highest sales occur in {top_month}."
-
     elif matched_intent == "total":
         df['Total'] = df['Price Per Unit'] * df['Quantity']
         total_sales = df['Total'].sum()
-        return f"The total sales amount is {total_sales:.2f}."
+        return f"The total sales amount is {total_sales:.2f}$"
 
-    return "I can answer questions about category, price, discount, payment, location, weekday, month, or total."
+    return "I can answer questions about category, price, discount, payment, location, or total."
 
 st.title("Retail Sales Chatbot")
 
